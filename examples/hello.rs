3f5c96365360ed47fec560c6fc7b9414de803d51
Erik Henriksson <erikhenrikssn@gmail.com>
2021-10-11 16:29:13 +0200

    Update to RTT and Rust 2021

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,20 +1,17 @@
-//! Prints "Hello, world!" on the host console using semihosting
+//! Prints "Hello, world!" on the host console using RTT
 
-#![no_main]
 #![no_std]
+#![no_main]
 
-use panic_halt as _;
-
+use panic_rtt_target as _;
+use rtt_target::{rtt_init_print, rprintln};
 use cortex_m_rt::entry;
-use cortex_m_semihosting::{debug, hprintln};
+use atsamd_hal as _;
 
 #[entry]
 fn main() -> ! {
-    hprintln!("Hello, world!").unwrap();
-
-    // exit QEMU
-    // NOTE do not run this on hardware; it can corrupt OpenOCD state
-    debug::exit(debug::EXIT_SUCCESS);
-
-    loop {}
+    rtt_init_print!();
+    loop {
+        rprintln!("Hello, world!");
+    }
 }
