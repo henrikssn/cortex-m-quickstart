ee097fdc5ccd197e5dee893415599fd6f16b9b22
Erik Henriksson <erikhenrikssn@gmail.com>
2021-10-11 16:51:51 +0200

    Adapt to SAM platform

--- examples/hello.rs
+++ examples/hello.rs
@@ -6,7 +6,6 @@
 use panic_rtt_target as _;
 use rtt_target::{rtt_init_print, rprintln};
 use cortex_m_rt::entry;
-use atsamd_hal as _;
 
 #[entry]
 fn main() -> ! {
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@
 use panic_rtt_target as _;
 use rtt_target::{rtt_init_print, rprintln};
 use cortex_m_rt::entry;
-use atsamd_hal as _;
 
 #[entry]
 fn main() -> ! {
